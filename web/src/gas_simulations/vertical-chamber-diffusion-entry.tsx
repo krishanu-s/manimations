c3fb360f72f0ce817031e3063cf4198f82f7d366
Krishanu <k@ksankar.me>
2026-07-10 17:43:14 -0700

    cleanup

--- web/src/gas_simulations/vertical-chamber-diffusion-entry.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import VerticalChamberDiffusion from "./VerticalChamberDiffusion";
-
-const root = document.getElementById("vertical-chamber-diffusion-root");
-if (root) createRoot(root).render(<VerticalChamberDiffusion />);
